c0ebe1e23f4f4d75e299177157c2a42b1abf8220
r7sh7 <bolinjkar.rishi@gmail.com>
2025-02-04 18:49:48 +0530

    Initialised the Eureka Server

---
+++ src/test/java/com/scaler/servicediscoveryjan25/ServiceDiscoveryJan25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.servicediscoveryjan25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryJan25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
